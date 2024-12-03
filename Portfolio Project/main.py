0cd772ebe68beade39695aa3ce503b5507f45c64
Andrew Ortiz <andrewortiz029@outlook.com>
2024-12-03 10:16:29 -0800

    Add files via upload

---
+++ Portfolio Project/main.py
@@ -0,0 +1,33 @@
+# import flast module
+from flask import Flask, render_template
+
+# instance of flask application
+app = Flask(__name__)
+
+# home route that returns below text when root url is accessed
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/skills")
+def skills():
+    return render_template('skills.html')
+
+@app.route("/education")
+def education():
+    return render_template('education.html')
+
+@app.route("/experience")
+def experience():
+    return render_template('experience.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':  
+   app.run()  
